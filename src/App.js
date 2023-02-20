e3cb1d4eb704bbb76518a2c122ae53327d7106d1
NaveenprasanthT <naveenprasanth2109@gmail.com>
2023-02-20 07:39:09 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'materialize-css/dist/css/materialize.css';
+import M from 'materialize-css/dist/js/materialize.js';
 import './App.css';
+import { Translation } from 'react-i18next';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AuthState from './context/auth/AuthState';
+import ProfileState from './context/profile/ProfileState';
+import ProductState from './context/product/ProductState';
+import FeedbackState from './context/feedback/FeedbackState';
+import ReviewState from './context/review/ReviewState';
+
+import Landing from './components/landing/Landing';
+import SignUp from './components/auth/SignUp';
+import SignIn from './components/auth/SignIn';
+import Admin from './components/admin/Admin';
+import Consumer from './components/consumer/Consumer';
+import Producer from './components/producer/Producer';
+import Profile from './components/profile/Profile';
+import Navbar from './components/layout/Navbar';
+
+import setAuthToken from './utils/setAuthToken';
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
 }
+const App = (props) => {
+  useEffect(() => {
+    M.AutoInit();
+  }, []);
+  const { i18n } = props;
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+  return (
+    <AuthState>
+      <ProfileState>
+        <ProductState>
+          <FeedbackState>
+            <ReviewState>
+              <Navbar changeLanguage={changeLanguage} />
+              <Router>
+                <Routes>
+                  <Route exact path='/' component={Landing} />
+                  <Route exact path='/signup' component={SignUp} />
+                  <Route exact path='/signin' component={SignIn} />
+                  <Route exact path='/admin' component={Admin} />
+                  <Route exact path='/profile' component={Profile} />
+                  <Route exact path='/consumer' component={Consumer} />
+                  <Route exact path='/producer' component={Producer} />
+                </Routes>
+              </Router>
+            </ReviewState>
+          </FeedbackState>
+        </ProductState>
+      </ProfileState>
+    </AuthState>
+  );
+};
 
-export default App;
+export default Translation('Translations')(App);
